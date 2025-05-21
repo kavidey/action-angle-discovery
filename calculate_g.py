78c52c9c7fef6efed23fe65d46540c161273defe
kavidey <kaviasher@gmail.com>
2025-05-21 17:54:05 +0000

    Clean up fourier transform code

--- calculate_g.py
+++ calculate_g.py
@@ -1,3 +1,5 @@
+# %%
+from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -8,6 +10,15 @@ import rebound as rb
 import celmech as cm
 from celmech.miscellaneous import frequency_modified_fourier_transform as fmft
 
+outer_only = True
+print(f"Outer Planets Only: {outer_only}")
+integration_path = Path("/home/kdey/action-angle-discovery/integrations") / ("outer_planets" if outer_only else "full_system")
+# %%
+file_names = list(integration_path.glob("*.sa"))
+
+transform_path = integration_path / "plots"
+transform_path.mkdir(parents=True, exist_ok=True)
+# %%
 # Read the table with the defined column specifications
 df = pd.read_fwf('MPCORB.DAT', colspecs=[[0,7], [8,14], [15,19], [20,25], [26,35], [36,46], [47, 57], [58,68], [69,81], [82, 91], [92, 103]])
 df = df[df['Epoch'] == 'K239D'] # take only ones at common epoch--almost all of them
@@ -17,14 +28,8 @@ for c in ['a', 'e', 'Incl.', 'Node', 'Peri.', 'M']:
 	df[c] = pd.to_numeric(df[c])
 
 labels = pd.read_fwf('proper_catalog24.dat', colspecs=[[0,10], [10,18], [19,28], [29,37], [38, 46], [47,55], [56,66], [67,78], [79,85], [86, 89], [90, 97]], header=None, index_col=False, names=['propa', 'da', 'prope', 'de', 'propsini', 'dsini', 'g', 's', 'H', 'NumOpps', "Des'n"])
-
 merged_df = pd.merge(df, labels, on="Des'n", how="inner")
-
-integration_path = "/home/kdey/action-angle-discovery/integrations/outer_planets/"
-file_names = os.listdir(integration_path)
-
-transform_path = "/home/lshen/fourier_integration_results/"
-
+# %%
 def k_vec_generation(max_order = 3, size = 7):
 	kvecs = []
 	
@@ -40,18 +45,22 @@ def k_vec_generation(max_order = 3, size = 7):
 	kvecs = np.delete(kvecs, indices, axis=0)
 	return kvecs
 
+def closest_key_entry(d, target):
+	closest_key = min(d.keys(), key=lambda k: abs(k - target))
+	return closest_key, d[closest_key]
+	
 k_vecs = k_vec_generation(max_order = 3, size = 7)
-k_vecs
 
 def Fourier_transform(filename):
-	results = cm.nbody_simulation_utilities.get_simarchive_integration_results(integration_path + filename,coordinates='heliocentric')
-	row = merged_df[merged_df["Des'n"] == filename[21:26]]
-	def closest_key_entry(d, target):
-		closest_key = min(d.keys(), key=lambda k: abs(k - target))
-		return closest_key, d[closest_key]
+	desn = filename.stem.split('_')[-1]
+	row = merged_df[merged_df["Des'n"] == desn]
+
+	results = cm.nbody_simulation_utilities.get_simarchive_integration_results(str(filename) ,coordinates='heliocentric')
+	
 	results['X'] = np.sqrt(2*(1-np.sqrt(1-results['e']**2))) * np.exp(1j * results['pomega'])
 	results['Y'] = (1-results['e']**2)**(0.25) * np.sin(0.5 * results['inc'] )* np.exp(1j * results['Omega'])
-	inner_planet_offset = 0
+	
+	inner_planet_offset = 0 if outer_only else 4
 
 	planets = ("Jupiter","Saturn","Uranus","Neptune", "Asteroid")
 	ecc_fmft_results = dict()
@@ -64,6 +73,7 @@ def Fourier_transform(filename):
 		inc_fmft_results[pl] = fmft(results['time'],results['Y'][i + inner_planet_offset],8)
 		planet_inc_freqs = np.array(list(inc_fmft_results[pl].keys()))
 		planet_inc_freqs_arcsec_per_yr = planet_inc_freqs * 60*60*180/np.pi * (2*np.pi)
+	
 	ARCSEC_PER_YR = 1/(180*60*60*2)
 	g_vec = np.zeros(4)
 	s_vec = np.zeros(3)
@@ -120,7 +130,7 @@ def Fourier_transform(filename):
 		x_dicts.append(x_dict)
 	
 	# The plots: 
-	fig,ax = plt.subplots(5 ,2,sharex=True,figsize=(10,10))
+	fig, ax = plt.subplots(5 ,2,sharex=True,figsize=(10,10))
 	Xsolns = []
 	Ysolns = []
 	for i,pl in enumerate(planets):
@@ -128,7 +138,7 @@ def Fourier_transform(filename):
 		ax[i,0].plot(results['time'],np.real(results['X'][i + inner_planet_offset]))
 		zsoln = np.sum([np.abs(amp) * np.exp(1j*((np.array(k)@omega_vec)*results['time'] + + np.angle(amp))) for k,amp in x_dicts[i].items()],axis=0)
 		ax[i,0].plot(results['time'],np.real(zsoln),'k--')
-	plt.savefig(transform_path + "plots/" + filename + ".png")
+	plt.savefig(transform_path / (filename.stem + ".png"))
 
 	freq_amp_dict = ecc_fmft_results['Asteroid']
 	zsoln = np.sum([np.abs(amp) * np.exp(1j*((np.array(k)@omega_vec)*results['time'] + np.angle(amp))) for k,amp in x_dicts[-1].items()],axis=0)
@@ -137,14 +147,14 @@ def Fourier_transform(filename):
 	asteroid_e_freqs = np.array(list(ecc_fmft_result.keys()))
 	asteroid_e_freqs_arcsec_per_yr = asteroid_e_freqs * 60*60*180/np.pi * (2*np.pi)
 
-	g_list = [filename, row['g'].values[0]]
-	for g in asteroid_e_freqs[:4]:
+	g_list = [desn, row['g'].values[0]]
+	for g in asteroid_e_freqs_arcsec_per_yr[:4]:
 		g_list.append(g)
 
 	return g_list
-
+# %%
 with Pool(20) as p:
 	g_table = p.map(Fourier_transform, file_names)
-	df = pd.DataFrame(g_table)
-	df.to_csv("Fourier Transform results.csv")
-	
+	df = pd.DataFrame(g_table, columns=["Des'n", 'g', 'g_1', 'g_2', 'g_3', 'g_4'])
+	df.to_csv(integration_path / f"asteroid_g.csv", index=False)
+# %%
